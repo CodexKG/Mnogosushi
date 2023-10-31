d84c6dcbeca1662a2cf574cd0ba997281382f3a7
Toktorov <ktoktorov2005@gmail.com>
2023-10-31 18:14:47 +0600

    product search

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path 
 
-from apps.products.views import product_detail
+from apps.products.views import product_detail, foods, search
 
 urlpatterns = [
-    path('<int:id>/', product_detail, name='product_detail')
+    path('<int:id>/', product_detail, name='product_detail'),
+    path('foods/', foods, name='foods'),
+    path('search/', search, name='search'),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 
 from apps.settings.models import Setting
 from apps.products.models import Product
@@ -8,3 +9,19 @@ def product_detail(request, id):
     setting = Setting.objects.latest('id')
     product = Product.objects.get(id=id)
     return render(request, 'products/detail.html', locals())
+
+def foods(request):
+    setting = Setting.objects.latest('id')
+    print("Setting", setting)
+    products = Product.objects.all().order_by('?')
+    return render(request, 'products/foods.html', locals())
+
+def search(request):
+    setting = Setting.objects.latest('id')
+    query = request.POST.get('query', '')
+
+    if query:
+        # Используйте Q-объекты для выполнения поиска в моделях Shop и Product
+        products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'products/foods.html', locals())
