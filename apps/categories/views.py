2b2b3254feaf313b90f840f3c159a4fb121bf485
KulmamatovZalkar <kulmamatov.zalkar@gmail.com>
2023-12-08 19:51:43 +0600

    Add cart fixed button to pages

--- apps/categories/views.py
+++ apps/categories/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render
 from apps.settings.models import Setting
 from apps.categories.models import Category
 from apps.products.models import Product
+from apps.carts.models import CartItem
+from django.db.models import Sum
 
 # Create your views here.
 def category_detail(request, slug):
     setting = Setting.objects.latest('id')
     category = Category.objects.get(slug=slug)
     products = Product.objects.filter(category=category)
+
+
     return render(request, 'category/detail.html', locals())
