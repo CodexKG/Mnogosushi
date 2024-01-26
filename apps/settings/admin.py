a96281d43fbc678202cef7d5fd69a79c4b66fb43
Toktorov <ktoktorov2005@gmail.com>
2024-01-26 11:54:33 +0600

    city modal select

--- apps/settings/admin.py
+++ apps/settings/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 
-from apps.settings.models import Setting, Contact, FAQ, Promotions, PromoCode
+from apps.settings.models import Setting, SettingCity, Contact, FAQ, Promotions, PromoCode
 
 # Register your models here.
+class SettingCityTabularInline(admin.TabularInline):
+    model = SettingCity
+    extra = 1
+
 @admin.register(Setting)
 class SettingAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
+    inlines = (SettingCityTabularInline, )
+
+@admin.register(SettingCity)
+class SettingCityAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url')
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
